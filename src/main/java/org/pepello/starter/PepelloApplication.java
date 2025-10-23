5acd6061e962ebc9749182b0b1031bd9fe509e92
tiryaki <tiryakiemre18@gmail.com>
2025-10-23 16:17:17 +0300

    proje yapısı belirlendi

---
+++ src/main/java/org/pepello/starter/PepelloApplication.java
@@ -0,0 +1,13 @@
+package org.pepello.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PepelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PepelloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pepello/starter/PepelloApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pepello.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PepelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
