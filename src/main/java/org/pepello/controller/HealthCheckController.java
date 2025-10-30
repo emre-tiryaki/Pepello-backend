be5910c031f5fc9a89cbbfeebd7003d93a1f0b55
tiryaki <tiryakiemre18@gmail.com>
2025-10-30 13:34:32 +0300

    çalışma kontrolü endpointi yazıldı

---
+++ src/main/java/org/pepello/controller/HealthCheckController.java
@@ -0,0 +1,12 @@
+package org.pepello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health-check")
+    public boolean healthCheck(){
+        return true;
+    }
+}
