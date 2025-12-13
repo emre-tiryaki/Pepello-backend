16381d6860ca2e7c3f240c7c358c664388de72f7
tiryaki <tiryakiemre18@gmail.com>
2025-12-13 20:51:02 +0300

    user endpointleri bitti

--- src/main/java/org/pepello/controller/Impl/UserControllerImpl.java
+++ src/main/java/org/pepello/controller/Impl/UserControllerImpl.java
@@ -4,17 +4,83 @@ import org.pepello.common.ICrud;
 import org.pepello.common.controller.BaseCrudController;
 import org.pepello.common.mapper.BaseMapper;
 import org.pepello.controller.IUserController;
+import org.pepello.dto.task.DtoTask;
+import org.pepello.dto.team.DtoTeam;
 import org.pepello.dto.user.DtoUser;
 import org.pepello.dto.user.UserCreateRequest;
+import org.pepello.dto.user.UserSearchRequest;
 import org.pepello.dto.user.UserUpdateRequest;
 import org.pepello.entities.User;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.pepello.mappers.TaskMapper;
+import org.pepello.mappers.TeamMapper;
+import org.pepello.mappers.UserMapper;
+import org.pepello.service.impl.TaskAsigneeRelationServiceImpl;
+import org.pepello.service.impl.UserServiceImpl;
+import org.pepello.service.impl.UserTeamRelationServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/user")
 public class UserControllerImpl extends BaseCrudController<User, DtoUser, UserCreateRequest, UserUpdateRequest> implements IUserController {
+    @Autowired
+    private UserTeamRelationServiceImpl userTeamRelationService;
+    @Autowired
+    private TeamMapper teamMapper;
+    @Autowired
+    private TaskAsigneeRelationServiceImpl taskAsigneeRelationService;
+    @Autowired
+    private TaskMapper taskMapper;
+    @Autowired
+    private UserMapper userMapper;
+    private final UserServiceImpl userService;
+
     public UserControllerImpl(ICrud<User, UserCreateRequest, UserUpdateRequest> service, BaseMapper<User, DtoUser> mapper) {
         super(service, mapper);
+        this.userService = (UserServiceImpl) service;
+    }
+
+    @GetMapping("/me")
+    public DtoUser getMe(
+            @RequestAttribute("userId")UUID userId
+    ) {
+        User user = service.getById(userId);
+
+        return mapper.toDto(user);
+    }
+
+    @GetMapping("/{userId}/teams")
+    public List<DtoTeam> getUsersTeams(
+            @PathVariable UUID userId
+    ) {
+        return userTeamRelationService
+                .getRelatedEntities(userId)
+                .stream()
+                .map(teamMapper::toDto)
+                .toList();
+    }
+
+    @GetMapping("/{userId}/tasks")
+    public List<DtoTask> getUsersTasks(
+            @PathVariable UUID userId
+    ) {
+        return taskAsigneeRelationService
+                .getPrimaryEntities(userId)
+                .stream()
+                .map(taskMapper::toDto)
+                .toList();
+    }
+
+    @GetMapping("/search")
+    public List<DtoUser> searchUser(
+            @RequestBody UserSearchRequest request
+    ) {
+        return userService.searchUser(request)
+                .stream()
+                .map(userMapper::toDto)
+                .toList();
     }
 }
--- src/main/java/org/pepello/repository/UserRepository.java
+++ src/main/java/org/pepello/repository/UserRepository.java
@@ -2,8 +2,11 @@ package org.pepello.repository;
 
 import org.pepello.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -12,4 +15,7 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE lower(concat(coalesce(u.firstName, ''), ' ', coalesce(u.lastName, ''))) LIKE lower(concat('%', :name, '%'))")
+    List<User> findByFullName(@Param("name") String name);
 }
