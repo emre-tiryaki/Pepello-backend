826a3c2af86b50ce2276d582f3736c5ca529d6a7
tiryaki <tiryakiemre18@gmail.com>
2025-12-23 16:53:53 +0300

    healtn-check'e erişilebilir artık.

--- src/main/java/org/pepello/config/SecurityConfig.java
+++ src/main/java/org/pepello/config/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/health-check").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
