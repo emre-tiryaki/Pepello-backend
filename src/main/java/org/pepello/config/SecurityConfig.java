d8c1a1007bb595b95a46d3c0cefc9ad149f6b0e8
tiryaki <tiryakiemre18@gmail.com>
2025-12-27 13:16:11 +0300

    import sildim

--- src/main/java/org/pepello/config/SecurityConfig.java
+++ src/main/java/org/pepello/config/SecurityConfig.java
@@ -16,7 +16,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
